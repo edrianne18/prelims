22a1ed765194f82104045492a7a1007152b3e449
Patrick Calagos <edriannecalagos@gmail.com>
2019-12-19 20:33:11 +0800

    added files

---
+++ dialog.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<title>dialog.html</title>
+	<link rel="stylesheet" type="text/css" href="dialog.css">
+</head>
+
+<body>
+	<div class="container">
+		<div class="box">
+			<div class="box1">Name</div>
+			
+			<div class="box2">ID No.</div>
+			
+			<div class="box3">Grade</div>
+			
+			<div class="input">
+				<div class="input1">
+				<input type="text" size="55"></input>
+				</div>
+				<div class="input2">
+				<input type="text" size="55"></input>
+				</div>
+				<div class="input3">
+				<select class="select">
+					<option></option>
+					<option>1.0</option>
+					<option>1.25</option>
+					<option>1.5</option>
+				</select>
+				</div>
+			<button class="btn">Save</button>
+			</div>
+			
+		</div>
+	</div>
+
+	
+</body>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<title>index.html</title>
+	<link rel="stylesheet" type="text/css" href="index.css">
+</head>
+
+<body>
+	<div class="btn1">
+		<button class="button btn1">Fill Table</button>
+	</div>
+	<div class="btn2">
+		<button class="button btn2">Fill Dialog</button>
+	</div>
+	<div class="container">
+	</div>
+</body>
